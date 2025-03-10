42b2d57ab9ec25ffe01d7401bef6ea6b67a0670f
Natalia Kurthan <kurthannatalia@gmail.com>
2025-03-11 02:53:51 +0300

    Fix_MongoDB_connection_issue

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { getEnvVar } from '../utils/getEnvVar.js';
+
+dotenv.config();
 
 export const initMongoConnection = async () => {
     try {
@@ -8,12 +11,21 @@ export const initMongoConnection = async () => {
         const url = getEnvVar('MONGODB_URL');
         const db = getEnvVar('MONGODB_DB');
 
-        await mongoose.connect(
-            `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`
-        );
+        if (!user || !pwd || !url || !db) {
+            throw new Error('Missing required MongoDB environment variables');
+        }
+
+        const uri = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+        console.log('Connecting to MongoDB:', uri);
+
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
         console.log('Mongo connection successfully established!');
     } catch (error) {
-        console.log('Error while setting up mongo connection', error);
+        console.error('Error while setting up mongo connection', error);
         process.exit(1);
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { setupServer } from './server.js';
+console.log("MongoDB URL:", process.env.MONGODB_URL);
+console.log("MongoDB USER:", process.env.MONGODB_USER);
+console.log("MongoDB PASSWORD:", process.env.MONGODB_PASSWORD);
+console.log("MongoDB DB:", process.env.MONGODB_DB);
 import { initMongoConnection } from './db/initMongoConnection.js';
 import 'dotenv/config';
 
