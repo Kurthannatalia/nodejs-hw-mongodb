d40809e5066a2ffd3b226cf67945559032ea52c9
Natalia Kurthan <kurthannatalia@gmail.com>
2025-03-11 01:02:38 +0300

    Added_src_files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+import 'dotenv/config';
+
+const appLauncher = async () => {
+    try {
+        await initMongoConnection();
+        setupServer();
+    } catch (error) {
+        console.error('Error launching the app:', error.message);
+        process.exit(1); 
+    }
+};
+
+appLauncher();
---
+++ src/services/contacts.js
@@ -0,0 +1,14 @@
+import {
+    Contact
+}
+from '../models/contact.js';
+
+export const getAllContacts = async () => {
+    const result = await Contact.find();
+    return result;
+};
+export const getContactById = async (contactId) => {
+    const result = await Contact.findById(contactId);
+    return result;
+};
+
