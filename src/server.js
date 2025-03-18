38a8e2849815164e4de58e60a80c9fd9c98261e3
Natalia Kurthan <kurthannatalia@gmail.com>
2025-03-18 22:23:39 +0300

    UPD_src_files

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-
 import contactsRouter from './routers/contacts.js';
-
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-
 const PORT = Number(getEnvVar('PORT', '3000'));
-
 export const setupServer = () => {
   const app = express();
 
