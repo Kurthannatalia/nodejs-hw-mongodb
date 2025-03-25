78166fcb0a1e32608fadb45112a81fa683107a13
Natalia Kurthan <kurthannatalia@gmail.com>
2025-03-25 23:57:24 +0300

    fix_bug

--- src/server.js
+++ src/server.js
@@ -7,25 +7,28 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import contactsRouter from './routers/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
+
 export const setupServer = () => {
-const app = express();
+    const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(
-    pino({
-        transport: {
-            target: 'pino-pretty',
-        },
-    }),
-);
+    app.use(express.json());
+    app.use(cors());
+    app.use(
+        pino({
+            transport: {
+                target: 'pino-pretty',
+            },
+        }),
+    );
 
-app.use('/contacts', contactsRouter);
+    app.use('/contacts', contactsRouter);
 
-app.use('*', notFoundHandler);
-app.use(errorHandler);
+    app.use('*', notFoundHandler);
+    app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`✅ Server is running on port ${PORT}`);
+    });
 };
+
+setupServer();
