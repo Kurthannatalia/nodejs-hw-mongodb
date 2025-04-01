1feecd0b16975fd8984fa77ae562eca109db6399
Natalia Kurthan <kurthannatalia@gmail.com>
2025-04-01 20:24:47 +0300

    Fix_syntax_error

--- src/models/contact.js
+++ src/models/contact.js
@@ -2,31 +2,32 @@ import mongoose from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate-v2';
 
 const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 20,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    isFavourite: {
-        type: Boolean,
-        default: false,
-    },
-    contactType: {
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  isFavourite: {
+    type: Boolean,
+    default: false,
+  },
+     contactType: {
         type: String,
         enum: ['work', 'personal'],
         default: 'personal',
     },
-} 
-  { versionKey: false, timestamps: true },
-);
+}, 
+{ versionKey: false, timestamps: true });
+
 const Contact = mongoose.model('Contact', contactSchema);
+
 export default Contact;
