ad63a62bf281ee50028789b238884d39199b59a6
Natalia Kurthan <kurthannatalia@gmail.com>
2025-04-02 23:41:47 +0300

    add_task

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,8 +1,18 @@
+import { HttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-    const { message = "Something went wrong", status = 500 } = err;
-    res.status(status).json({
-        status,
-        message,
-        data: message,
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
     });
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
 };
--- src/models/contact.js
+++ src/models/contact.js
@@ -1,33 +1,101 @@
-import mongoose from 'mongoose';
-import mongoosePaginate from 'mongoose-paginate-v2';
+import createHttpError from 'http-errors';
+import * as contactsServices from '../services/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
-const contactSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 20,
-    },
-    phoneNumber: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    isFavourite: {
-        type: Boolean,
-        default: false,
-    },
-    contactType: {
-        type: String,
-        enum: ['work', 'personal'],
-        default: 'personal',
-    },
-});
+const getAllContactsController = async (req, res) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
+  const { _id: userId } = req.user;
 
-contactSchema.plugin(mongoosePaginate);
+  const totalContacts = await contactsServices.countContacts({ userId, filter });
 
-const Contact = mongoose.model('Contact', contactSchema);
-export default Contact;
+  const contacts = await contactsServices.getAllContacts({
+    userId,
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: {
+      docs: contacts,
+      totalDocs: totalContacts,
+      totalPages: Math.ceil(totalContacts / perPage),
+      hasNextPage: page * perPage < totalContacts,
+      hasPrevPage: page > 1,
+    },
+  });
+};
+
+const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const { _id: userId } = req.user;
+  const contact = await contactsServices.getContactById({ contactId, userId });
+
+  if (!contact) {
+    throw createHttpError(404, 'Contact not found');
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
+
+const createContactController = async (req, res) => {
+  const { _id: userId } = req.user;
+  const contact = await contactsServices.createContact({ ...req.body, userId });
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
+};
+
+const updateContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const { _id: userId } = req.user;
+  const result = await contactsServices.updateContact(
+    contactId,
+    userId,
+    req.body,
+  );
+
+  if (!result) throw createHttpError(404, 'Contact not found');
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
+};
+
+const deleteContactController = async (req, res) => {
+  const { contactId } = req.params;
+  const { _id: userId } = req.user;
+  const result = await contactsServices.deleteContact({ contactId, userId });
+
+  if (!result) throw createHttpError(404, 'Contact not found');
+
+  res.status(204).send();
+};
+
+const ctrl = {
+  getAllContactsController: ctrlWrapper(getAllContactsController),
+  getContactByIdController: ctrlWrapper(getContactByIdController),
+  createContactController: ctrlWrapper(createContactController),
+  updateContactController: ctrlWrapper(updateContactController),
+  deleteContactController: ctrlWrapper(deleteContactController),
+};
+
+export default ctrl;
