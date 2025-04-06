28e9ab70aee53e08ee4df94a5f195fe4803525e1
Natalia Kurthan <kurthannatalia@gmail.com>
2025-04-07 01:33:13 +0300

    upd_models

--- src/models/contact.js
+++ src/models/contact.js
@@ -36,40 +36,6 @@ const contactSchema = new Schema(
   { versionKey: false, timestamps: true },
 );
 
-contactSchema.statics.findContactById = async function (contactId, userId) {
-  const contact = await this.findOne({ _id: contactId, userId });
-  if (!contact) {
-    throw createHttpError(404, 'Contact not found');
-  }
-  return contact;
-};
-
-contactSchema.statics.createNewContact = async function (payload) {
-  const newContact = new this(payload);
-  await newContact.save();
-  return newContact;
-};
-
-contactSchema.statics.updateContact = async function (contactId, userId, payload) {
-  const updatedContact = await this.findOneAndUpdate(
-    { _id: contactId, userId },
-    payload,
-    { new: true }
-  );
-  if (!updatedContact) {
-    throw createHttpError(404, 'Contact not found');
-  }
-  return updatedContact;
-};
-
-contactSchema.statics.deleteContact = async function (contactId, userId) {
-  const deletedContact = await this.findOneAndDelete({ _id: contactId, userId });
-  if (!deletedContact) {
-    throw createHttpError(404, 'Contact not found');
-  }
-  return deletedContact;
-};
-
 const Contact = model('Contact', contactSchema);
 
 export { Contact };
