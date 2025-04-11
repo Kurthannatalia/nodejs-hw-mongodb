c9b52379be78af7af1e59fb8531fcab0d28d18e4
Natalia Kurthan <kurthannatalia@gmail.com>
2025-04-11 15:25:31 +0300

    upd_validation

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,4 +1,3 @@
-////////////////////////////////////////////////////////////////////
 import Joi from 'joi';
 ////////////////////////////////////////////////////////////////////
 export const registerUserSchema = Joi.object({
@@ -15,9 +14,8 @@ export const loginUserSchema = Joi.object({
 export const requestResetEmailSchema = Joi.object({
   email: Joi.string().email().required(),
 });
-
+////////////////////////////////////////////////////////////////////
 export const resetPasswordSchema = Joi.object({
   password: Joi.string().required(),
   token: Joi.string().required(),
 });
-//////////
